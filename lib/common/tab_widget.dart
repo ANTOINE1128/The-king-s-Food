f821b075c407302ce1afed5b24b965602b4a1d59
Antoine Makdessy <makdessyantoine@gmail.com>
2024-06-14 22:20:30 +0300

    initial commit

--- lib/common/tab_widget.dart
+++ lib/common/tab_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+
 class TabWidget extends StatelessWidget {
   const TabWidget({super.key, required this.text});
 
